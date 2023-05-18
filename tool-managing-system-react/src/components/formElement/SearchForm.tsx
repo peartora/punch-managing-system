e2685d40e621d3d66946be971fd9e34d5a0aafce
Lee Sangmin (DaeP/MOE2) <lsm1dae@bosch.com>
2023-05-18 18:12:53 +0900

    updating search functions

--- tool-managing-system-react/src/components/formElement/SearchForm.tsx
+++ tool-managing-system-react/src/components/formElement/SearchForm.tsx
@@ -1,22 +1,43 @@
+import { useState } from "react";
+
+
+
 function SearchForm() {
+
+  const [productType, setProductType] = useState("");
+
+
   return (
-    
     <form>
       <h3>조회할 data를 입력 하세요</h3>
       <div className="row">
-        <div className="col">
+        {/* <div className="col">
           <label className="form-label">펀치 ID: </label>
           <input className="form-control" type="text" placeholder="펀치 ID" />
+        </div> */}
+
+        <div className="col">
+          <label htmlFor="startDate" className="form-label">시작 날짜: </label>
+          <input id="startDate" className="form-control" type="date" placeholder="시작 날짜" />
         </div>
 
         <div className="col">
-          <label className="form-label">등록날짜: </label>
-          <input className="form-control" type="date" placeholder="등록날짜" />
+          <label htmlFor="endDate" className="form-label">종료 날짜: </label>
+          <input id="endDate"className="form-control" type="date" placeholder="종료 날짜" />
         </div>
 
         <div className="col">
-          <label className="form-label">펀치 타입: </label>
-          <input className="form-control" type="email" placeholder="펀치 타입" />
+          <label htmlFor="productType" className="form-label">타입: </label>
+          <select
+            id="productType"
+            className="form-control"
+            value="조회할 type을 선택 하세요"
+            onChange={(event) => setProductType(event.target.value)}
+          >
+            <option value="A">A</option>
+            <option value="B">B</option>
+            <option value="C">C</option>
+          </select>
         </div>
 
         <div className="col">
@@ -60,6 +81,8 @@ function SearchForm() {
           />
         </div>
       </div>
+      <input type="submit" value="조회" />
+
     </form>
   );
 }
