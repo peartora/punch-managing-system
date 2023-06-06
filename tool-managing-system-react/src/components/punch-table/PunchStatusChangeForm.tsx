e0652fb3146fec8de1a7ec11c81f51d479c00e11
peartora2 <practiceforgit@gmail.com>
2023-06-06 13:52:14 +0900

    함께 상태변경 시 조건 기능 추가

--- tool-managing-system-react/src/components/punch-table/PunchStatusChangeForm.tsx
+++ tool-managing-system-react/src/components/punch-table/PunchStatusChangeForm.tsx
@@ -1,7 +1,9 @@
 import { request } from "@/common/Service";
+import { type PunchRow as PunchRowType } from "@/common/types";
 
 type Props = {
   selectedIds: Array<string>;
+  punchRowsById: Record<string, PunchRowType>;
   refetch: () => void;
 };
 
@@ -27,6 +29,36 @@ export default function PunchStatusChangeForm(props: Props) {
       if (result) {
         const targetRows: Record<string, unknown>[] = props.selectedIds.map(
           (id) => {
+            if (
+              props.punchRowsById[id].punchStatus === "사용대기" ||
+              props.punchRowsById[id].punchStatus === "사용불가" ||
+              props.punchRowsById[id].punchStatus === "폐기"
+            ) {
+              alert(
+                `사용대기, 사용불가, 폐기 상태의 펀치는 해당 기능으로 상태 변경 할 수 없습니다.
+                (해당 되지 않는 펀치도 상태변경 되지 않습니다.)`
+              );
+              return {};
+            } else if (
+              props.punchRowsById[id].punchStatus === "사용가능" &&
+              (newStatus === "사용대기" || newStatus === "사용가능")
+            ) {
+              alert(
+                `사용가능 상태의 펀치는 사용대기 혹은 사용가능 상태로 변경 될 수 없습니다.
+                (해당 되지 않는 펀치도 상태변경 되지 않습니다.)`
+              );
+              return {};
+            } else if (
+              props.punchRowsById[id].punchStatus === "사용중" &&
+              (newStatus === "사용대기" || newStatus === "사용중")
+            ) {
+              alert(
+                `사용중 상태의 펀치는 사용대기 혹은 사용중 상태로 변경 될 수 없습니다.
+                (해당 되지 않는 펀치도 상태변경 되지 않습니다.)`
+              );
+              return {};
+            }
+
             return {
               punchId: id,
               newStatus: newStatus,
