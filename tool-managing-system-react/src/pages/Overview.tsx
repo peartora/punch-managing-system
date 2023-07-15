d5b7db38b1c6edee6116266358a9986ead6a9c73
peartora2 <practiceforgit@gmail.com>
2023-07-15 13:18:20 +0900

    펀치 상세 내용 추가

--- tool-managing-system-react/src/pages/Overview.tsx
+++ tool-managing-system-react/src/pages/Overview.tsx
@@ -8,48 +8,58 @@ const OverViewList = () => {
   let countForIndex = 0;
   let keys: string[] = [];
 
-  console.log("result before even for each state");
-  console.log(result);
-
   rows.forEach((r) => {
-    console.log("result just after foreach state");
-    console.log(result);
+    // 1-2023-07-09-가네진-상부
+    const punchId = r.punchId;
+    const keyArray: string[] = punchId.split("-");
+    const registerDate = `${keyArray[1]}-${keyArray[2]}-${keyArray[3]}`;
+    const key = `${keyArray[4]}-${registerDate}`;
 
-    const product = r.product;
     const punchType = r.punchType;
 
-    if (keys.indexOf(product) === -1) {
-      keys = [...keys, product];
-    }
+    if (keys.indexOf(key) === -1) {
+      console.log("key");
+      console.log(key);
 
-    console.log("result before if state");
-    console.log(result);
+      keys = [...keys, key];
+
+      console.log("keys");
+      console.log(keys);
+    }
 
     if (result.length === 0) {
       if (punchType === "상부") {
-        result[0] = { [product]: { 상부: 1, 하부: 0, 다이: 0 } };
+        result[0] = {
+          [key]: { 등록날짜: registerDate, 상부: 1, 하부: 0, 다이: 0 },
+        };
       } else if (punchType === "하부") {
-        result[0] = { [product]: { 상부: 0, 하부: 1, 다이: 0 } };
+        result[0] = {
+          [key]: { 등록날짜: registerDate, 상부: 0, 하부: 1, 다이: 0 },
+        };
       } else {
-        result[0] = { [product]: { 상부: 0, 하부: 0, 다이: 1 } };
+        result[0] = {
+          [key]: { 등록날짜: registerDate, 상부: 0, 하부: 0, 다이: 1 },
+        };
       }
       countForIndex++;
     } else {
       result.forEach((resultObject: any, i: number) => {
-        if (product in resultObject) {
-          console.log("product값이 존재 합니다.");
+        if (key in resultObject) {
           if (punchType === "상부") {
-            result[i][product]["상부"]++;
+            result[i][key]["상부"]++;
           } else if (punchType === "하부") {
-            result[i][product]["하부"]++;
+            result[i][key]["하부"]++;
           } else {
-            result[i][product]["다이"]++;
+            result[i][key]["다이"]++;
           }
         } else {
-          console.log("product값이 존재 하지 않습니다.");
-
           if (keys.length > result.length) {
-            const newObject = { 상부: 0, 하부: 0, 다이: 0 };
+            const newObject = {
+              등록날짜: registerDate,
+              상부: 0,
+              하부: 0,
+              다이: 0,
+            };
             if (punchType === "상부") {
               newObject["상부"] = 1;
             } else if (punchType === "하부") {
@@ -58,7 +68,7 @@ const OverViewList = () => {
               newObject["다이"] = 1;
             }
 
-            result[countForIndex] = { [product]: newObject };
+            result[countForIndex] = { [key]: newObject };
             countForIndex++;
           } else {
             return;
@@ -76,6 +86,7 @@ const OverViewList = () => {
       <thead>
         <tr>
           <th>제품</th>
+          <th>펀치 등록날짜</th>
           <th>상부 펀치 등록 개수</th>
           <th>하부 펀치 등록 개수</th>
           <th>다이 펀치 등록 개수</th>
@@ -87,8 +98,9 @@ const OverViewList = () => {
           const productName = products[0];
 
           return (
-            <tr key={productName}>
+            <tr key={`${productName}-${r[productName]["등록날짜"]}`}>
               <td>{productName}</td>
+              <td>{r[productName]["등록날짜"]}</td>
               <td>{r[productName]["상부"]}</td>
               <td>{r[productName]["하부"]}</td>
               <td>{r[productName]["다이"]}</td>
