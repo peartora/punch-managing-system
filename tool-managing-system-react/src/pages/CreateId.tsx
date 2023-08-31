c765812b22156bba9a2c8d35549b3734594a6ff2
peartora <peartora@gmail.com>
2023-08-31 22:05:21 +0900

    비밀번호 4자리 이상

--- tool-managing-system-react/src/pages/CreateId.tsx
+++ tool-managing-system-react/src/pages/CreateId.tsx
@@ -15,46 +15,52 @@ export function CreateId() {
     e.preventDefault();
 
     if (password === passwordConfirmation) {
-      const body = {
-        id,
-      };
+      if (password.length >= 4) {
+        const body = {
+          id,
+        };
 
-      request
-        .post(`/api/tool-managing-system/duplicate_username`, body)
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error(`${id}의 중복 확인 중 error 발생 하였습니다.`);
-          }
-          return response.text();
-        })
-        .then((result) => {
-          if (result === "OK") {
-            const body = {
-              id,
-              password,
-            };
+        request
+          .post(`/api/tool-managing-system/duplicate_username`, body)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`${id}의 중복 확인 중 error 발생 하였습니다.`);
+            }
+            return response.text();
+          })
+          .then((result) => {
+            if (result === "OK") {
+              const body = {
+                id,
+                password,
+              };
 
-            request
-              .post(`/api/tool-managing-system/create_id`, body)
-              .then((response) => {
-                if (!response.ok) {
-                  throw new Error(`회원가입 중 network error 발생 하였습니다.`);
-                }
-                return response.text();
-              })
-              .then((result) => {
-                if (result === "OK") {
-                  alert(`${id}의 회원가입이 완료 되었습니다.`);
-                  navigate(`/sign-in`);
-                } else {
-                  alert(`${id}의 회원가입 중 error가 발생 하였습니다.`);
-                }
-              })
-              .catch((error) => console.error(error));
-          } else {
-            alert(`${id}는 이미 존재 하는 id 입니다.`);
-          }
-        });
+              request
+                .post(`/api/tool-managing-system/create_id`, body)
+                .then((response) => {
+                  if (!response.ok) {
+                    throw new Error(
+                      `회원가입 중 network error 발생 하였습니다.`
+                    );
+                  }
+                  return response.text();
+                })
+                .then((result) => {
+                  if (result === "OK") {
+                    alert(`${id}의 회원가입이 완료 되었습니다.`);
+                    navigate(`/sign-in`);
+                  } else {
+                    alert(`${id}의 회원가입 중 error가 발생 하였습니다.`);
+                  }
+                })
+                .catch((error) => console.error(error));
+            } else {
+              alert(`${id}는 이미 존재 하는 id 입니다.`);
+            }
+          });
+      } else {
+        alert(`password의 길이는 최소 4자리 이상 입니다.`);
+      }
     } else {
       alert(`입력한 2개의 비밀번호가 일치 하지 않습니다.`);
     }
