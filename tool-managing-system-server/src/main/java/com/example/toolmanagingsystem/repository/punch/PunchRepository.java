ac9cf99a5c43a99ef9788d6eeb87967ffd3613bc
Lee Sangmin (DaeP/MOE2) <lsm1dae@bosch.com>
2024-06-12 18:21:56 +0900

    0612 - office - final

--- tool-managing-system-server/src/main/java/com/example/toolmanagingsystem/repository/punch/PunchRepository.java
+++ tool-managing-system-server/src/main/java/com/example/toolmanagingsystem/repository/punch/PunchRepository.java
@@ -15,7 +15,10 @@ public interface PunchRepository extends JpaRepository<Punch, Long>, JpaSpecific
 {
     Punch findByPunchId(String punchId);
 
-    @Query("select p from punch p")
+//    @Query("select p from punch p")
+//    List<Punch> findAllPunch();
+
+    @Query("select p from punch p join CleanHistory c on p.punchId = c.punchNumber")
     List<Punch> findAllPunch();
 
     @Query("select p from punch p where " +
